76aeb2a6d05810bdedc7a7f66854cf26b42b16da
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-14 20:37:37 -0300

    Update concatenacao_com_virula.py

--- concatenacao_com_virula.py
+++ concatenacao_com_virula.py
@@ -1,1 +1,2 @@
 print('O resultado da soma entre 2 + 3 é' , 2 + 3)
+# Aqui o uso de vírgula se torna obrigatório pois a linguagem espera que a STRING anterior "some" com uma próxima STRING (mesmo tipo), e não um INT.
