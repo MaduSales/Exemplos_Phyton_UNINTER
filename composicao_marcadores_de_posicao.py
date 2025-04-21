63c978c66447c5666a8e549759dda10e914669ef
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-21 20:46:28 -0300

    Update composicao_marcadores_de_posicao.py

--- composicao_marcadores_de_posicao.py
+++ composicao_marcadores_de_posicao.py
@@ -1,3 +1,10 @@
 nota = 8
 s1 = 'Sua nota foi %i na disciplina de Algoritmos' % nota
 print(s1)
+
+
+# Outro exemplo: 
+nota = 8.5
+disciplina = 'Algoritmos'
+s1 = 'Sua nota foi %d na disciplina de %s' % (nota, disciplina)
+print(s1)
