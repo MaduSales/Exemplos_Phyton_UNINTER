a65c11a9eec583ae28940c475f788f84d7aefba3
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-25 14:07:14 -0300

    Create udo_do_if.py

---
+++ udo_do_if.py
@@ -0,0 +1,10 @@
+# Lê dois valores inteiros e compara ambos
+x = int(input('Digite um valor: '))
+y = int(input('Digite outro valor: '))
+
+if (x > y):
+  print('{} é maior que o {}' .format(x,y))
+if (x < y):
+  print('{} é maior que o {}' .format(y,x))
+if (x == y):
+  print('Os dois números são iguais')
