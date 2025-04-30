114390929b7a0b334241a5de58f47a391f368e60
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-30 13:14:51 -0300

    Create operadores_logicos.py

---
+++ operadores_logicos.py
@@ -0,0 +1,22 @@
+# Operador Lógico - NOT
+x = True
+y = False
+print(not x)
+print(not y)
+
+
+# Operador Lógico - AND
+x = True
+y = True
+print(x and y)
+
+# ==================
+x = False
+y = True
+print(x and y)
+
+
+# Operador Lógico - OR
+x = False
+y = True
+print(x or y)
