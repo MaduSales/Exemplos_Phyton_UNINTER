643fd2c8cb27814e553ed66126feaf0997f567de
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-15 16:28:38 -0300

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,11 @@
+# Listas
+mochila = ['Machado', 'Camisa','Abacate']
+print(mochila)
+
+# São mutáveis
+mochila[2] = 'Laranja'
+print(mochila)
+
+# Adicionar elementos:
+mochila.append('Ovos')
+print(mochila)
