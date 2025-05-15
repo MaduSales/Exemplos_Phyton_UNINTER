c3a5743862be9e76fa47be1b12aeda0a43502177
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-15 16:38:32 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -9,3 +9,15 @@ print(mochila)
 # Adicionar elementos:
 mochila.append('Ovos')
 print(mochila)
+
+# Inserir elementos em índices específicos (desloca elementos para o próximo índice)
+mochila.insert(3,'Canivete')
+print(mochila)
+
+# Remover por índice
+del mochila[1]
+print(mochila)
+
+# Remover por valor
+mochila.remove('Canivete')
+print(mochila)
