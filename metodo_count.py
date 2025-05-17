8d6e62dea101e2fc5916d8067f91251b3a35c567
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-17 15:25:22 -0300

    Create metodo_count.py

---
+++ metodo_count.py
@@ -0,0 +1,7 @@
+# Método count
+s1 = 'O rato roeu a roupa do rei de roma'
+s2 = 'O pelo no peito do pé do Pedro é preto'
+s3 = 'Um mafagafinho, dois mafagafinhos, três mafagafinhos'
+print(s1.count('r'))
+print(s2.lower().count('p'))
+print(s3.count('mafagafinho'))
